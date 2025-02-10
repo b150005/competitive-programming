c627f396dcfdc639c81eac5671d3013cd902b607
b150005 <b150005@outlook.jp>
2025-02-10 11:18:44 +0900

    ✨ abs-abc086c

--- abs/abc086c/abs-abc086c.ts
+++ abs/abc086c/abs-abc086c.ts
@@ -19,4 +19,50 @@ To submit, run the following command:
 
 import { readFileSync } from "node:fs";
 
+type Node = {
+  t: number;
+  x: number;
+  y: number;
+};
+
 const inputs = readFileSync("/dev/stdin", "utf-8").trimEnd();
+const initialNode = {
+  t: 0,
+  x: 0,
+  y: 0,
+};
+const nodes: Node[] = [
+  initialNode,
+  ...inputs
+    .split("\n")
+    .slice(1)
+    .map((str) => {
+      const nums = str.split(" ").map((str) => Number.parseInt(str));
+      return { t: nums[0], x: nums[1], y: nums[2] };
+    }),
+];
+
+function canMove(from: Node, to: Node): boolean {
+  const dt = Math.abs(to.t - from.t);
+  const dx = Math.abs(to.x - from.x);
+  const dy = Math.abs(to.y - from.y);
+  const dd = dx + dy;
+
+  if (dd <= dt && dt % 2 === dd % 2) {
+    return true;
+  }
+  return false;
+}
+
+function check(nodes: Node[]): boolean {
+  for (let i = 0; i < nodes.length - 1; i++) {
+    const { from, to } = { from: nodes[i], to: nodes[i + 1] };
+    if (!canMove(from, to)) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+console.log(check(nodes) ? "Yes" : "No");
