900bebc5fde35d5c2a9515178d379e6fcc2d6592
b150005 <b150005@outlook.jp>
2025-02-10 10:18:22 +0900

    ✨ abs-abc086a

--- abs/abc086a/abs-abc086a.ts
+++ abs/abc086a/abs-abc086a.ts
@@ -20,3 +20,13 @@ To submit, run the following command:
 import { readFileSync } from "node:fs";
 
 const inputs = readFileSync("/dev/stdin", "utf-8").trimEnd();
+const [a, b] = inputs.split(" ").map((str) => Number.parseInt(str));
+
+function isProductOdd(a: number, b: number): boolean {
+  if (a % 2 !== 0 && b % 2 !== 0) {
+    return true;
+  }
+  return false;
+}
+
+console.log(isProductOdd(a, b) ? "Odd" : "Even");
