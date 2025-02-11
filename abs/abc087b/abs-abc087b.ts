7701b08444466ef60a939f846f44821fee6243ba
b150005 <b150005@outlook.jp>
2025-02-11 10:22:30 +0900

    ✨ abs-abc087b

--- abs/abc087b/abs-abc087b.ts
+++ abs/abc087b/abs-abc087b.ts
@@ -20,3 +20,30 @@ To submit, run the following command:
 import { readFileSync } from "node:fs";
 
 const inputs = readFileSync("/dev/stdin", "utf-8").trimEnd();
+const [a, b, c, x] = inputs.split("\n").map((str) => Number.parseInt(str));
+
+/*
+  500s + 100t + 50u = x
+  0 <= s <= a
+  0 <= t <= b
+  0 <= u <= c
+*/
+function countCoins(a: number, b: number, c: number, total: number): number {
+  let count = 0;
+  for (let s = 0; s <= a; s++) {
+    for (let t = 0; t <= b; t++) {
+      for (let u = 0; u <= c; u++) {
+        const sum = 500 * s + 100 * t + 50 * u;
+        if (sum === total) {
+          count += 1;
+        } else if (sum > total) {
+          break;
+        }
+      }
+    }
+  }
+
+  return count;
+}
+
+console.log(countCoins(a, b, c, x).toString());
