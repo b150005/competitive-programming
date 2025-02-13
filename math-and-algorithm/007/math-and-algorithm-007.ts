ee606a9d17eaaf637f5d4de57f7d9bc396c97090
b150005 <b150005@outlook.jp>
2025-02-14 08:52:24 +0900

    ✨ math-and-algorithm-007

--- math-and-algorithm/007/math-and-algorithm-007.ts
+++ math-and-algorithm/007/math-and-algorithm-007.ts
@@ -20,3 +20,46 @@ To submit, run the following command:
 import { readFileSync } from "node:fs";
 
 const inputs = readFileSync("/dev/stdin", "utf-8").trimEnd();
+const [n, x, y] = inputs.split(" ").map((str) => Number.parseInt(str));
+
+/**
+ * 指定された範囲とステップに基づいて整数の配列を生成します。
+ *
+ * @param start 配列の開始値(範囲の最初の値)
+ * @param end 配列の終了値(範囲の最後の値を含む)
+ * @param step 配列内の値の間隔(デフォルト: `1`)
+ * @returns 指定された範囲とステップで生成された整数配列
+ *
+ * @example
+ * // 1から10まで1ずつ増加する配列を生成
+ * createArray(1, 10); // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+ *
+ * @example
+ * // 0から10まで2ずつ増加する配列を生成
+ * createArray(0, 10, 2); // [0, 2, 4, 6, 8, 10]
+ *
+ * @example
+ * // 負のステップで10から1まで減少する配列を生成
+ * createArray(10, 1, -1); // [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+ */
+function createArray(start: number, end: number, step = 1): number[] {
+  return Array.from({ length: (end - start) / step + 1 }, (_, index) => start + index * step);
+}
+
+function gcd(num1: number, num2: number): number {
+  if (num2 === 0) {
+    return num1;
+  }
+  return gcd(num2, num1 % num2);
+}
+
+function lcm(num1: number, num2: number): number {
+  return num1 * (num2 / gcd(num1, num2));
+}
+
+const nums = createArray(1, n);
+const _lcm = lcm(x, y);
+const multiplesOfX = nums.filter((num) => num % x === 0).length;
+const multiplesOfY = nums.filter((num) => num % y === 0).length;
+const multiplesOfXY = nums.filter((num) => num % _lcm === 0).length;
+console.log((multiplesOfX + multiplesOfY - multiplesOfXY).toString());
