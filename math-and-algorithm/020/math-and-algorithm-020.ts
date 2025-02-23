1f9653f97dedd37be3162f9007b22439e111373c
b150005 <b150005@outlook.jp>
2025-02-23 14:25:09 +0900

    ✨ math-and-algorithm-020

--- math-and-algorithm/020/math-and-algorithm-020.ts
+++ math-and-algorithm/020/math-and-algorithm-020.ts
@@ -20,3 +20,29 @@ To submit, run the following command:
 import { readFileSync } from "node:fs";
 
 const inputs = readFileSync("/dev/stdin", "utf-8").trimEnd();
+const as = inputs
+  .split("\n")[1]
+  .split(" ")
+  .map((str) => Number.parseInt(str));
+
+function combination(nums: number[]): number {
+  let result = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      for (let k = j + 1; k < nums.length; k++) {
+        for (let l = k + 1; l < nums.length; l++) {
+          for (let m = l + 1; m < nums.length; m++) {
+            if (nums[i] + nums[j] + nums[k] + nums[l] + nums[m] === 1000) {
+              result++;
+            }
+          }
+        }
+      }
+    }
+  }
+
+  return result;
+}
+
+console.log(combination(as).toString());
