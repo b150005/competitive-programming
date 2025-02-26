124e6fd63d4f6683cba778bec7df5ec562b505fe
b150005 <b150005@outlook.jp>
2025-02-26 17:29:44 +0900

    ✨ math-and-algorithm-025

--- math-and-algorithm/025/math-and-algorithm-025.ts
+++ math-and-algorithm/025/math-and-algorithm-025.ts
@@ -20,3 +20,21 @@ To submit, run the following command:
 import { readFileSync } from "node:fs";
 
 const inputs = readFileSync("/dev/stdin", "utf-8").trimEnd();
+const as = inputs
+  .split("\n")[1]
+  .split(" ")
+  .map((str) => Number.parseInt(str));
+const bs = inputs
+  .split("\n")[2]
+  .split(" ")
+  .map((str) => Number.parseInt(str));
+
+function expectation(nums1: number[], nums2: number[]): number {
+  let total = 0;
+  for (let i = 0; i < nums1.length; i++) {
+    total = total + (nums1[i] + 2 * nums2[i]) / 3;
+  }
+  return total;
+}
+
+console.log(expectation(as, bs).toString());
